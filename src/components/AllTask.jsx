408432f95aabb2717cb45a862ca75a1fddd933d7
Prajwol Mainali <prajwolmainali96@gmail.com>
2025-04-08 08:53:18 +0545

    update

--- src/components/AllTask.jsx
+++ src/components/AllTask.jsx
@@ -6,20 +6,42 @@ import { TiTick } from "react-icons/ti";
 const AllTask = () => {
   const [addData, setAddData] = useState([]);
   const [inputText, setInputText] = useState("");
+  const [editIndex, setEditIndex] = useState(null);
+  const [editText, setEditText] = useState("");
 
   function handleInput(e) {
     setInputText(e.target.value);
   }
 
+  // function to add the item
   function setData() {
     if (inputText.trim() === "") return; // Prevent adding empty strings
     setAddData((prev) => [...prev, inputText]);
     setInputText("");
   }
 
+  // function to delete the item
   function deleteData(index) {
-    console.log("Item deleted");
     setAddData((prev) => prev.filter((_, i) => i !== index));
+    if (index === editIndex) {
+      setEditIndex("");
+    }
+  }
+
+  //function to edit the item
+  function editData(index) {
+    setEditIndex(index);
+    setEditText(addData[index]);
+  }
+
+  //save the edit item
+
+  function saveEditData() {
+    if (editText.trim() === "") return;
+    const updateData = [...addData];
+    updateData[editIndex] = editText;
+    setAddData(updateData);
+    setEditText("");
   }
 
   return (
@@ -40,11 +62,32 @@ const AllTask = () => {
 
       {addData.map((data, index) => (
         <div key={index}>
-          <div className="flex justify-between items-center bg-gray-200 p-4 mt-4 rounded-lg">
-            <h1 className="text-2xl font-bold text-gray-700">{data}</h1>
-            <div className="flex gap-4">
-              <TiTick />
-              <CiEdit />
+          <div className="flex justify-between items-start bg-gray-200 p-4 mt-4 rounded-lg ">
+            {editIndex === index ? (
+              <input
+                type="text"
+                value={editText}
+                onChange={(e) => setEditText(e.target.value)}
+                className="p-4 text-black font-bold bg-gray-200 rounded-lg"
+                placeholder="Edit your task"
+              ></input>
+            ) : (
+              <h1 className="text-2xl font-bold text-gray-700 w-[380px] break-words pr-4">
+                {data}
+              </h1>
+            )}
+            <div className="flex gap-3 flex-shrink-0">
+              {editIndex === index ? (
+                <TiTick
+                  className="h-[24px] w-[24px] cursor-pointer text-green-600"
+                  onClick={() => saveEditData(index)}
+                />
+              ) : (
+                <CiEdit
+                  className="h-[24px] w-[24px] cursor-pointer"
+                  onClick={() => editData(index)}
+                />
+              )}
               <MdDeleteForever
                 onClick={() => deleteData(index)}
                 className="cursor-pointer h-[24px] w-[24px] text-red-500"
